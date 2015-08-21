8a6cdccddef6c43a34b65f1b3eade14ba966625f
Axelandre42 <axelandre42@yahoo.fr>
2015-08-21 23:17:42 +0200

    Starting the first block ! It will be used for have many sub-blocks.

---
+++ src/main/java/net/axelandre42/technologies/common/block/BlockAlpha.java
@@ -0,0 +1,13 @@
+package net.axelandre42.technologies.common.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockAlpha extends Block {
+
+	public BlockAlpha() {
+		super(Material.rock);
+		
+	}
+
+}
