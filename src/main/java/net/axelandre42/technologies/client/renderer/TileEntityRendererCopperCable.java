4f6de654a863ec9147241f4639c0ce6b14ac464b
Axelandre42 <axelandre42@yahoo.fr>
2015-09-06 12:39:32 +0200

    Continuing the model of the copper cable !

--- src/main/java/net/axelandre42/technologies/client/renderer/TileEntityRendererCopperCable.java
+++ src/main/java/net/axelandre42/technologies/client/renderer/TileEntityRendererCopperCable.java
@@ -3,6 +3,7 @@ package net.axelandre42.technologies.client.renderer;
 import org.lwjgl.opengl.GL11;
 
 import net.axelandre42.technologies.Axelandre42Technologies;
+import net.axelandre42.technologies.common.tileentity.TileEntityCopperCable;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
@@ -18,20 +19,82 @@ public class TileEntityRendererCopperCable extends TileEntitySpecialRenderer {
 	@Override
 	public void renderTileEntityAt(TileEntity tileentity, double translationX,
 			double translationY, double translationZ, float f) {
-		// TODO Auto-generated method stub
 		GL11.glTranslated(translationX, translationY, translationZ);
 		GL11.glDisable(GL11.GL_LIGHTING);
 		this.bindTexture(texture);
-		drawCore(tileentity);
+		{
+			drawCore();
+			
+			TileEntityCopperCable coppercable = (TileEntityCopperCable) tileentity;
+			for (int i = 0; i < coppercable.connectors.length; i++) {
+				if (coppercable.connectors[i] != null)
+					drawConnector(coppercable.connectors[i]);
+			}
+		}
 		GL11.glEnable(GL11.GL_LIGHTING);
 		GL11.glTranslated(-translationX, -translationY, -translationZ);
 	}
 	
 	private void drawConnector(ForgeDirection direction) {
+		GL11.glTranslatef(0.5F, 0.5F, 0.5F);
+		if (direction.equals(ForgeDirection.NORTH)) {
+			GL11.glRotatef(270, 1, 0, 0);
+		} else if (direction.equals(ForgeDirection.SOUTH)) {
+			GL11.glRotatef(90, 1, 0, 0);
+		} else if (direction.equals(ForgeDirection.EAST)) {
+			GL11.glRotatef(270, 0, 0, 1);
+		} else if (direction.equals(ForgeDirection.WEST)) {
+			GL11.glRotatef(90, 0, 0, 1);
+		} else if (direction.equals(ForgeDirection.DOWN)) {
+			GL11.glRotatef(180, 1, 0, 0);
+		} else if (direction.equals(ForgeDirection.UP)) {
+			
+		}
+		GL11.glTranslatef(-0.5F, -0.5F, -0.5F);
 		
+		Tessellator tessellator = Tessellator.instance;
+		tessellator.startDrawingQuads();
+		{
+			tessellator.addVertexWithUV(1 - 10 * pixel / 2, 1 - 10 * pixel / 2, 1 - 10 * pixel / 2, 12 * texturePixel, 5 * texturePixel);
+			tessellator.addVertexWithUV(1 - 10 * pixel / 2, 1, 1 - 10 * pixel / 2, 12 * texturePixel, 0 * texturePixel);
+			tessellator.addVertexWithUV(10 * pixel / 2, 1, 1 - 10 * pixel / 2, 6 * texturePixel, 0 * texturePixel);
+			tessellator.addVertexWithUV(10 * pixel / 2, 1 - 10 * pixel / 2, 1 - 10 * pixel / 2, 6 * texturePixel, 5 * texturePixel);
+			
+			tessellator.addVertexWithUV(10 * pixel / 2, 1 - 10 * pixel / 2, 10 * pixel / 2, 12 * texturePixel, 5 * texturePixel);
+			tessellator.addVertexWithUV(10 * pixel / 2, 1, 10 * pixel / 2, 12 * texturePixel, 0 * texturePixel);
+			tessellator.addVertexWithUV(1 - 10 * pixel / 2, 1, 10 * pixel / 2, 6 * texturePixel, 0 * texturePixel);
+			tessellator.addVertexWithUV(1 - 10 * pixel / 2, 1 - 10 * pixel / 2, 10 * pixel / 2, 6 * texturePixel, 5 * texturePixel);
+			
+			tessellator.addVertexWithUV(1 - 10 * pixel / 2, 1 - 10 * pixel / 2, 10 * pixel / 2, 12 * texturePixel, 5 * texturePixel);
+			tessellator.addVertexWithUV(1 - 10 * pixel / 2, 1, 10 * pixel / 2, 12 * texturePixel, 0 * texturePixel);
+			tessellator.addVertexWithUV(1 - 10 * pixel / 2, 1, 1 - 10 * pixel / 2, 6 * texturePixel, 0 * texturePixel);
+			tessellator.addVertexWithUV(1 - 10 * pixel / 2, 1 - 10 * pixel / 2, 1 - 10 * pixel / 2, 6 * texturePixel, 5 * texturePixel);
+			
+			tessellator.addVertexWithUV(10 * pixel / 2, 1 - 10 * pixel / 2, 1 - 10 * pixel / 2, 12 * texturePixel, 5 * texturePixel);
+			tessellator.addVertexWithUV(10 * pixel / 2, 1, 1 - 10 * pixel / 2, 12 * texturePixel, 0 * texturePixel);
+			tessellator.addVertexWithUV(10 * pixel / 2, 1, 10 * pixel / 2, 6 * texturePixel, 0 * texturePixel);
+			tessellator.addVertexWithUV(10 * pixel / 2, 1 - 10 * pixel / 2, 10 * pixel / 2, 6 * texturePixel, 5 * texturePixel);
+		}
+		tessellator.draw();
+		
+		GL11.glTranslatef(0.5F, 0.5F, 0.5F);
+		if (direction.equals(ForgeDirection.NORTH)) {
+			GL11.glRotatef(-270, 1, 0, 0);
+		} else if (direction.equals(ForgeDirection.SOUTH)) {
+			GL11.glRotatef(-90, 1, 0, 0);
+		} else if (direction.equals(ForgeDirection.EAST)) {
+			GL11.glRotatef(-270, 0, 0, 1);
+		} else if (direction.equals(ForgeDirection.WEST)) {
+			GL11.glRotatef(-90, 0, 0, 1);
+		} else if (direction.equals(ForgeDirection.DOWN)) {
+			GL11.glRotatef(-180, 1, 0, 0);
+		} else if (direction.equals(ForgeDirection.UP)) {
+			
+		}
+		GL11.glTranslatef(-0.5F, -0.5F, -0.5F);
 	}
 	
-	private void drawCore(TileEntity tileentity) {
+	private void drawCore() {
 		Tessellator tessellator = Tessellator.instance;
 		tessellator.startDrawingQuads();
 		{
--- src/main/java/net/axelandre42/technologies/common/tileentity/TileEntityCopperCable.java
+++ src/main/java/net/axelandre42/technologies/common/tileentity/TileEntityCopperCable.java
@@ -1,7 +1,51 @@
 package net.axelandre42.technologies.common.tileentity;
 
 import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.common.util.ForgeDirection;
 
 public class TileEntityCopperCable extends TileEntity {
-
+	
+	/**
+	 * UP, DOWN, NORTH, EAST, SOUTH, WEST.
+	 */
+	public ForgeDirection[] connectors = new ForgeDirection[6];
+	
+	public TileEntityCopperCable() {
+		
+	}
+	
+	@Override
+	public void updateEntity() {
+		this.updateConnectors();
+		
+	}
+	
+	private void updateConnectors() {
+		if (this.worldObj.getTileEntity(xCoord, yCoord + 1, zCoord) instanceof TileEntityCopperCable)
+			this.connectors[0] = ForgeDirection.UP;
+		else
+			this.connectors[0] = null;
+		if (this.worldObj.getTileEntity(xCoord, yCoord - 1, zCoord) instanceof TileEntityCopperCable)
+			this.connectors[1] = ForgeDirection.DOWN;
+		else
+			this.connectors[1] = null;
+		if (this.worldObj.getTileEntity(xCoord, yCoord, zCoord - 1) instanceof TileEntityCopperCable)
+			this.connectors[2] = ForgeDirection.NORTH;
+		else
+			this.connectors[2] = null;
+		if (this.worldObj.getTileEntity(xCoord + 1, yCoord, zCoord) instanceof TileEntityCopperCable)
+			this.connectors[3] = ForgeDirection.EAST;
+		else
+			this.connectors[3] = null;
+		if (this.worldObj.getTileEntity(xCoord, yCoord, zCoord + 1) instanceof TileEntityCopperCable)
+			this.connectors[4] = ForgeDirection.SOUTH;
+		else
+			this.connectors[4] = null;
+		if (this.worldObj.getTileEntity(xCoord - 1, yCoord, zCoord) instanceof TileEntityCopperCable)
+			this.connectors[5] = ForgeDirection.WEST;
+		else
+			this.connectors[5] = null;
+		
+	}
+	
 }
